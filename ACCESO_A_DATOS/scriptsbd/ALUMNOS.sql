7d84f3bf9841ff6e4b10b52beb108477fb625dfb
JESUS <JESUS@DESKTOP-1OQ40FH.home>
2021-11-16 21:17:08 +0100

    UD02 Acceso a BBDD relacionaeles - commit 02

---
+++ ACCESO_A_DATOS/scriptsbd/ALUMNOS.sql
@@ -0,0 +1,61 @@
+-- CREATE DATABASE PruebaConexionBD;
+-- USE PruebaConexionBD;
+create table ALUMNOS (
+	id INT PRIMARY KEY,
+	nombre VARCHAR(50),
+	apellido1 VARCHAR(50),
+	apellido2 VARCHAR(50),
+	email VARCHAR(50),
+	edad INT
+);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (1, 'Ingra', 'Cloke', 'Rouby', 'irouby0@wikipedia.org', 44);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (2, 'Susie', 'Widdop', null, 'sbourget1@liveinternet.ru', 66);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (3, 'Tracee', 'McQuilkin', null, 'tcuseck2@europa.eu', 84);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (4, 'Cleve', 'Camilleri', 'Godden', 'cgodden3@gizmodo.com', 72);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (5, 'Winona', 'Draper', null, 'warnaudi4@cpanel.net', 83);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (6, 'Heywood', 'Waldram', 'Bullier', 'hbullier5@people.com.cn', 88);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (7, 'Aron', 'Lydford', 'Dannel', 'adannel6@scientificamerican.com', 88);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (8, 'Rycca', 'Farquarson', null, 'rivashechkin7@unc.edu', 39);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (9, 'Eileen', 'Millar', null, 'eriddler8@constantcontact.com', 95);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (10, 'Kristoforo', 'Schimpke', 'Whittuck', 'kwhittuck9@microsoft.com', 41);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (11, 'Shanon', 'Pigden', 'Banat', 'sbanata@meetup.com', 74);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (12, 'Nolana', 'Leney', 'Bartell', 'nbartellb@intel.com', 20);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (13, 'Melba', 'Ringsell', 'Happs', 'mhappsc@nba.com', 42);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (14, 'Felecia', 'Gard', 'Fullerd', 'ffullerdd@ox.ac.uk', 99);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (15, 'Anselm', 'Mea', 'Bargh', 'abarghe@ycombinator.com', 95);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (16, 'Inness', 'Hawyes', null, 'imountjoyf@skyrock.com', 62);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (17, 'Ariadne', 'Scarlett', 'Sollowaye', 'asollowayeg@arstechnica.com', 73);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (18, 'Meredith', 'Fowle', null, 'mwibberleyh@bizjournals.com', 62);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (19, 'Tull', 'Tyres', 'Denisovo', 'tdenisovoi@cisco.com', 100);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (20, 'Clari', 'Hawkwood', 'Muddiman', 'cmuddimanj@shutterfly.com', 42);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (21, 'Dennie', 'Severs', 'Gainfort', 'dgainfortk@blogspot.com', 92);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (22, 'Micheil', 'Brimfield', 'Dolley', 'mdolleyl@tripadvisor.com', 64);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (23, 'Philippine', 'MacCague', 'Bister', 'pbisterm@freewebs.com', 42);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (24, 'Hasheem', 'Olivet', null, 'hmcinneryn@sciencedaily.com', 31);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (25, 'Lorrin', 'Sibbert', 'Cantos', 'lcantoso@un.org', 77);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (26, 'Carole', 'Suart', 'Eastwood', 'ceastwoodp@japanpost.jp', 95);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (27, 'Inga', 'Hemphall', null, 'ibreinleinq@networkadvertising.org', 77);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (28, 'Cherise', 'Bryceson', 'Baglow', 'cbaglowr@cnn.com', 94);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (29, 'Lynnet', 'Sisey', 'Daud', 'ldauds@engadget.com', 52);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (30, 'Freeman', 'Bittlestone', 'Arnatt', 'farnattt@diigo.com', 63);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (31, 'Rube', 'Stenton', 'Walworth', 'rwalworthu@npr.org', 40);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (32, 'Rosana', 'Feasey', 'Chelsom', 'rchelsomv@webnode.com', 61);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (33, 'Alika', 'Harcus', null, 'ameinsw@homestead.com', 66);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (34, 'Clarette', 'Webby', 'Murgatroyd', 'cmurgatroydx@fc2.com', 99);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (35, 'Ransom', 'Ibbotson', 'Rayner', 'rraynery@berkeley.edu', 63);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (36, 'Jimmie', 'Stive', 'Andrioni', 'jandrioniz@blinklist.com', 45);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (37, 'Rutger', 'Beel', 'Hillatt', 'rhillatt10@patch.com', 43);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (38, 'Sergei', 'Mattei', null, 'snorridge11@cpanel.net', 56);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (39, 'Zoe', 'Overnell', 'Salway', 'zsalway12@army.mil', 27);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (40, 'Robinia', 'Kinchlea', 'Kollasch', 'rkollasch13@ovh.net', 5);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (41, 'Staci', 'Larwell', 'Jonke', 'sjonke14@java.com', 40);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (42, 'Bethina', 'Boerderman', 'Millthorpe', 'bmillthorpe15@wufoo.com', 42);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (43, 'Irwinn', 'Leal', 'Smalls', 'ismalls16@ycombinator.com', 96);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (44, 'Ara', 'Flucker', 'Nazareth', 'anazareth17@xing.com', 72);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (45, 'Toni', 'Kilmurray', 'Dik', 'tdik18@comsenz.com', 21);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (46, 'Frasco', 'Cason', null, 'fpoundsford19@census.gov', 7);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (47, 'Morissa', 'Waren', 'Hemms', 'mhemms1a@icio.us', 43);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (48, 'Cordelie', 'Magrannell', 'Lesslie', 'clesslie1b@msu.edu', 60);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (49, 'Fifine', 'Henkmann', null, 'fhanhart1c@webeden.co.uk', 29);
+insert into ALUMNOS (id, nombre, apellido1, apellido2, email, edad) values (50, 'Joye', 'Swanborrow', null, 'jlequesne1d@posterous.com', 81);
+commit;
---
+++ ACCESO_A_DATOS/src/accdat/UD02/accesobdrelacionales/Ejercicio3.java
@@ -0,0 +1,66 @@
+/**
+ *
+ */
+package accdat.UD02.accesobdrelacionales;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author JESUS
+ *
+ */
+public class Ejercicio3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Connection con = null;
+
+		try {
+			// Si trabajaramos con JDBC < 4.0 tendríamos que indicar esta línea
+			// para indicar el tipo de driver que tiene que cargar DriverManager.
+			// Class.forName("com.mysql.jdbc.Driver");
+
+			// Obtenemos la conexión a partir de la URL jdbc correspondiente
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/PruebaConexionBD", "root", "");
+
+			// Obtenermos una instancia de un objeto que implementa la interface statement.
+			Statement st = con.createStatement();
+
+			String sql = "SELECT APELLIDO1, APELLIDO2, NOMBRE FROM ALUMNOS ORDER BY APELLIDO1, APELLIDO2, NOMBRE"; 
+
+			ResultSet rs = st.executeQuery(sql);
+
+			// De esta forma vamos a saber si hay tablas o no
+			boolean hayFilas = false;
+			while(rs.next()) {
+				hayFilas = true;
+				System.out.println(rs.getString("APELLIDO1") + " " + rs.getString("APELLIDO2") + ", " + rs.getString("NOMBRE"));
+			}
+			if (!hayFilas) {
+				System.out.println("No hay resultados que mostrar");
+			}
+
+			//Cerramos ResultSet y Statement
+			rs.close();
+			st.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+		}
+
+	}
+
+}
